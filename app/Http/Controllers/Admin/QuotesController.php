0baca5f7313041c0ace2fa1917aafe1a5e999272
Piero Desposorio <pdesposoriogonzales@gmail.com>
2021-07-06 07:49:21 -0500

    Prueba 15

--- app/Http/Controllers/Admin/QuotesController.php
+++ app/Http/Controllers/Admin/QuotesController.php
@@ -43,9 +43,9 @@ class QuotesController extends Controller
     public function edit(Quote $id, Request $request)
     {
         //$quote = Quote::with('service')->find($id);
-        //$quote = $id->whereIn('id', $request->id)->get();
+        $quote = $id->whereIn('id', $request->id)->get();
         //$quote = Quote::find($id);
-        $quote = DB::select('SELECT id, phone, email, description, state FROM quotes WHERE id IN ('.$request->id.')');
+        //$quote = DB::select('SELECT id, phone, email, description, state FROM quotes WHERE id IN ('.$request->id.')');
         //$quote = DB::table('quotes')->select('phone', 'email', 'description', 'state')->whereIn('id', $request->id)->get();
         return response()->json($quote);
     }
