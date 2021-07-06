88e1a18ebb76ee0f403715f53045d8ff40246d29
Piero Desposorio <pdesposoriogonzales@gmail.com>
2021-07-06 16:53:32 -0500

    Prueba 20

--- database/migrations/2021_07_04_232606_create_quotes_table.php
+++ database/migrations/2021_07_04_232606_create_quotes_table.php
@@ -14,7 +14,8 @@ class CreateQuotesTable extends Migration
     public function up()
     {
         Schema::create('quotes', function (Blueprint $table) {
-            $table->id();
+            //$table->id();
+            $table->integer('id', true, true);
             $table->string('client');
             $table->string('phone');
             $table->string('email');
