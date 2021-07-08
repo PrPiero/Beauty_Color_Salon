c4dbda14bd7a573f8af4cbf6142d0ee099ce2e47
joan <2017210253@untels.edu.pe>
2021-07-07 21:16:45 -0500

    cuarto commit

---
+++ vyncolor/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Livewire\ShoppingCart;
+
+
+Route::get('/', WelcomeController::class);
+Route::get('search', SearchController::class)->name('search');
+
+Route::get('categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
+
+Route::get('products/{product}',[ProductController::class,'show'])->name('products.show');
+
+Route::get('shopping-cart', ShoppingCart::class)->name('shopping-cart');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+
+Route::get('prueba', function () {
+    \Cart::destroy();
+});
